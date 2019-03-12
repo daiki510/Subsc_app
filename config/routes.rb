7368b6b749d3395e76dcefadd6c976b7d5925bf8
ddgg7755 <ddgg7755@gmail.com>
2019-03-12 16:49:51 +0900

    subscriptionsのルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'subscriptions/new'
-  get 'subscriptions/index'
-  get 'subscriptions/show'
-  get 'subscriptions/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :subscriptions
 end
