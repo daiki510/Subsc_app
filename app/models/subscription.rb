3806a51d343bb366ee63c673a7767fe8767cec77
ddgg7755 <ddgg7755@gmail.com>
2019-03-13 10:27:52 +0900

    アソシエーションの定義

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,6 @@
 class Subscription < ApplicationRecord
   enum status: { open: 0, secret: 9, development: 5 }
+
+  has_many :addtions, dependent: :destroy
+  has_many :added_users, through: :additions, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :addtions, dependent: :destroy
+  has_many :added_subscriptions, through: :additions, source: :subscription
 end
