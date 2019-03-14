736aa87be16f8ec5e335a0b46650176e283c1dd9
ddgg7755 <ddgg7755@gmail.com>
2019-03-14 16:26:28 +0900

    合計利用料金の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @subscriptions = @user.added_subscriptions
-    # @detail = Detail.find_by(user_id: current_user.id, subscription_id: subscription.id)
-    # raise
   end
-
-  # def detail(subscription)
-  #   current_user.detailed_subscriptions.find_by(id: subscription.id)
-  # end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def total_charge
+    @user.details.map { |detail| detail.charge}.sum
+  end
 end
