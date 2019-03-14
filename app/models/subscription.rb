c1a18971a4d302c09ca0802f82827cc92e6ea03f
ddgg7755 <ddgg7755@gmail.com>
2019-03-14 17:40:21 +0900

    subscriptionモデルにバリデーション追加

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,13 @@
 class Subscription < ApplicationRecord
+  #バリデーション
+  validates :name, presence: true, uniqueness: true, length: {maximum: 30}
+  validates :icon, length: {maximum: 255}
+  validates :summary, presence: true, length: {maximum: 255}
+  
+  #enumの定義
   enum status: { open: 0, secret: 9, development: 5 }
 
+  #アソシエーション
   has_many :additions, dependent: :destroy
   has_many :added_users, through: :additions, source: :user
 
