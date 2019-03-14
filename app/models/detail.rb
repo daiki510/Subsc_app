e100fbe2d51559b074797d2f5b23c7bcff0226ad
ddgg7755 <ddgg7755@gmail.com>
2019-03-14 15:58:20 +0900

    サブスクに対して詳細情報のないものは未登録と表示

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,8 +1,4 @@
 class Detail < ApplicationRecord
   belongs_to :user
   belongs_to :subscription
-
-  # def check_detail?(subscription)
-  #   self.find_by(user_id: current_user.id, subscription_id: subsription.id)
-  # end
 end
