af7956b2a896f308ef63be002f37d7416462a34c
ddgg7755 <ddgg7755@gmail.com>
2019-03-19 19:08:53 +0900

    マイページで支払日順にソート機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,27 +18,28 @@ class UsersController < ApplicationController
     #ソート機能
     @subscriptions = @subscriptions.order(name: :asc) if params[:sort_name] #名前順
     @subscriptions = only_not_has_detail(@subscriptions, @details) if params[:sort_not_has_detail] #詳細未登録のみ
-    @subscriptions = sort_charge(@details,@subscriptions) if params[:sort_charge] #利用料金順
-    # @subscriptions = current_user.added_subscriptions if params[:sort_charge]
-
+    @subscriptions = sort_charge(@details) if params[:sort_charge] #利用料金順
+    @subscriptions = sort_date(@details) if params[:sort_date] #支払日順
   end
 
   private
   #未登録のサブスクリプションを抽出
   def only_not_has_detail(subscriptions, details)
-    subsc_ids = subscriptions.map {|subsc| subsc.id}
-    detail_ids = details.map {|detail| detail.subscription_id}
-    not_has_detail_ids = subsc_ids - detail_ids
+    subsc_ids = subscriptions.map {|subsc| subsc.id} #ユーザーが追加したサブスクリプションのidを抽出
+    detail_ids = details.map {|detail| detail.subscription_id} #詳細が登録されているサブスクリプションのidを抽出
+    not_has_detail_ids = subsc_ids - detail_ids #両方の配列にないidを算出する
     subscriptions.where(id: not_has_detail_ids)
   end
-
-  def sort_charge(details,subscriptions)
+  
+  #料金順にソート
+  def sort_charge(details)
     detail_ids = details.order(charge: :desc).map {|detail| detail.subscription_id}
     Subscription.find(detail_ids).sort_by{ |o| detail_ids.index(o.id)}
   end
 
-  def sort_date(details,subscriptions)
-    # detail_ids = details.order(charge: :desc).map {|detail| detail.subscription_id}
-    # Subscription.find(detail_ids).sort_by{ |o| detail_ids.index(o.id)}
+  #支払順にソート
+  def sort_date(details)
+    detail_ids = details.order(due_date: :asc).map {|detail| detail.subscription_id}
+    Subscription.find(detail_ids).sort_by{ |o| detail_ids.index(o.id)}
   end
 end
