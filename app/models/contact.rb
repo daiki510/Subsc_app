9de1c8a2b5aa5257c200a1859bfd502b79410c22
ddgg7755 <ddgg7755@gmail.com>
2019-03-22 15:42:30 +0900

    contactモデルにバリデーション追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :message, presence: true, length: {maximum: 500}
 end
