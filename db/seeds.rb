bb80ce37c7b6546ea76fd6bf26870095a56ee057
ddgg7755 <ddgg7755@gmail.com>
2019-03-25 14:54:43 +0900

    シードデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create([
+  {name: '音楽'},
+  {name: '映画'},
+  {name: '動画'},
+  {name: 'ファッション'},
+  {name: '生活'},
+  {name: '本'},
+  {name: 'ゲーム'},
+  {name: '学習'},
+  {name: 'ツール'},
+  {name: 'クラウド'},
+  {name: '飲食'}
+])
+20.times do |i|
+  Subscription.create(
+    name: "サブスクリプション-#{i}",
+    icon: "アイコン-#{i}",
+    summary: "サマリー-#{i}"
+  )
+end
+1.times do |i|
+  User.create(
+    name: "test",
+    email: "test@test.com",
+    admin: true,
+    password:  "000000",
+    password_confirmation:  "000000"
+  )
+end
 
-# Category.create([
-#   {name: '音楽'},
-#   {name: '映画'},
-#   {name: '動画'},
-#   {name: 'ファッション'},
-#   {name: '生活'},
-#   {name: '本'},
-#   {name: 'ゲーム'},
-#   {name: '学習'},
-#   {name: 'ツール'},
-#   {name: 'クラウド'},
-#   {name: '飲食'}
-# ])
+10.times do |i|
+  User.create(
+    name: "admin-#{i}",
+    email: "admin-#{i}@admin.com",
+    admin: true,
+    password:  "000000",
+    password_confirmation:  "000000"
+  )
+end
 
 50.times do |i|
   User.create(
