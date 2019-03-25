982922b3aa6e2573618b65945c9fc130af643b22
ddgg7755 <ddgg7755@gmail.com>
2019-03-25 15:39:14 +0900

    sendgridの設定の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'subsc-app' }
+  config.action_mailer.default_url_options = { host: 'subsc-app.heroku.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
