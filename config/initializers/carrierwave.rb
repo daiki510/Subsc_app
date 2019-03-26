8528bb63024d182fafd3cadf4d1d880e989dadb6
ddgg7755 <ddgg7755@gmail.com>
2019-03-26 11:06:57 +0900

    S3の設定を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
 CarrierWave.configure do |config|
   # require 'carrierwave/storage/abstract'
   # require 'carrierwave/storage/file'
-  # require 'carrierwave/storage/fog'
+  require 'carrierwave/storage/fog'
 
-  # config.storage :fog
-  # config.cache_storage = :fog
+  config.storage :fog
+  config.cache_storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
-      # path_style:            true
+      path_style:            true
   }
 
   config.fog_public     = true
