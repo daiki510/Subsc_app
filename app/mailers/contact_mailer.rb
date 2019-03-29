993ea105247f5b7909c945a4fdbd8e2364ffc9fc
ddgg7755 <ddgg7755@gmail.com>
2019-03-29 17:44:26 +0900

    送信先の環境変数の修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: ENV['MAIL'], subject: "お問い合わせの確認メール"
+    mail to: ENV['GMAIL'], subject: "お問い合わせの確認メール"
   end
 end
