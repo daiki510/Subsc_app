1ac59608206b552d846e84c2d66ca93cae9c4676
ddgg7755 <ddgg7755@gmail.com>
2019-03-31 23:04:52 +0900

    定期ジョブの時間修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 # set :output, 'log/cron.log'
 set :output, { :error => 'log/whenever.log', :standard => 'log/cron.log' }
 
-set :environment, :development
-every 1.minute do
-  rake "notifying:notification"
-end
+# set :environment, :development
+# every 1.minute do
+#   rake "notifying:notification"
+# end
 
 set :environment, :production
 every 1.day, at: ['9:00 am', '11:00 pm'] do
