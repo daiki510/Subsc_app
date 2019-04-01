09822bb87624577215c8a893e81b709c2e17fa0d
ddgg7755 <ddgg7755@gmail.com>
2019-04-01 16:54:31 +0900

    userモデルのrspecをリファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user1, class: User do
+    name { "test_user1" }
+    email { "user1@example.com" }
+    password { "000000" }
+  end
+  factory :user2, class: User do
+    name { "test_user2" }
+    email { "user2@example.com" }
+    password { "000000" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user1 = FactoryBot.create(:user1)
+    @user2 = FactoryBot.create(:user2)
+  end
+  context "check factorybot of validation" do
+    # factory_botが有効かどうかを検査。
+    it "has a valid factory of user1" do
+      user = @user1
+      expect(user).to be_valid
+    end
+    it "has a valid factory of user2" do
+      user = @user2
+      expect(user).to be_valid
+    end
+  end
+
   # 名前、メールアドレス、パスワードがあれば有効な状態であること
   it "is valid with a name, email, password" do
-    user = User.new(
-      name: "test_name",
-      email: "test@example.com",
-      password: "000000",
-      password_confirmation: "000000"
-    )
+    user = @user1
     expect(user).to be_valid
   end
   # 名前がなければ無効な状態であること
@@ -19,61 +30,26 @@ RSpec.describe User, type: :model do
   end
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email" do
-    User.create(
-      name: "test_name1",
-      email: "test@example.com",
-      password: "000000",
-      password_confirmation: "000000"
-    )
-    user = User.new(
-      name: "test_name2",
-      email: "test@example.com",
-      password: "000000",
-      password_confirmation: "000000"
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("はすでに存在します")
+    user1 = @user1
+    user2 = FactoryBot.build(:user2, email: "user1@example.com")
+    user2.valid?
+    expect(user2.errors[:email]).to include("はすでに存在します")
   end
   # 名前が31文字以上なら無効な状態であること
   it "is invalid with name which has 31 or more characters" do
-    user = User.new(
-      name: "test"*10,
-      email: "test@example.com",
-      password: "000000",
-      password_confirmation: "000000"
-    )
+    user = FactoryBot.build(:user1, name: "user1"*10)
     user.valid?
     expect(user.errors[:name]).to include("は30文字以内で入力してください")
   end
   # パスワードが5文字以下なら無効な状態であること
   it "is invalid with password which has 5 or less characters" do
-    user = User.new(
-      name: "test",
-      email: "test@example.com",
-      password: "0000",
-      password_confirmation: "0000"
-    )
+    user = FactoryBot.build(:user1, password: "0000")
     user.valid?
     expect(user.errors[:password]).to include("は6文字以上で入力してください")
   end
-  # パスワードと確認パスワードが一致しなければ無効な状態であること
-  it "is invalid password and password_confimation do not match" do
-    user = User.new(
-      name: "test",
-      email: "test@example.com",
-      password: "000000",
-      password_confirmation: "111111"
-    )
-    expect(user).not_to be_valid
-  end
   # メールアドレスの書式が無効な状態であること
   it "is valid with email which is collect format" do
-    user = User.new(
-      name: "test_name",
-      email: "test_email",
-      password: "000000",
-      password_confirmation: "000000"
-    )
+    user = FactoryBot.build(:user1, email: "test_user1")
     user.valid?
     expect(user.errors[:email]).to include("は不正な値です")
   end
