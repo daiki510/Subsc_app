4aea169f64afbfb1d444a2d8a4101d3fcfada746
ddgg7755 <ddgg7755@gmail.com>
2019-04-01 15:44:02 +0900

    userモデルのRspecの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # 名前と概要があれば有効な状態であること
-  it "is valid with a name, summary" do
+  # 名前、メールアドレス、パスワードがあれば有効な状態であること
+  it "is valid with a name, email, password" do
     user = User.new(
       name: "test_name",
-      summary: "test_summary"
+      email: "test@example.com",
+      password: "000000",
+      password_confirmation: "000000"
     )
     expect(user).to be_valid
   end
@@ -15,54 +17,64 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:name]).to include("を入力してください")
   end
-  # 重複した名前なら無効な状態であること
-  it "is invalid with a duplicate name" do
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email" do
     User.create(
-      name: "test_name",
-      summary: "test_summary1"
+      name: "test_name1",
+      email: "test@example.com",
+      password: "000000",
+      password_confirmation: "000000"
     )
     user = User.new(
-      name: "test_name",
-      summary: "test_summary2"
+      name: "test_name2",
+      email: "test@example.com",
+      password: "000000",
+      password_confirmation: "000000"
     )
     user.valid?
-    expect(user.errors[:name]).to include("はすでに存在します")
+    expect(user.errors[:email]).to include("はすでに存在します")
   end
   # 名前が31文字以上なら無効な状態であること
   it "is invalid with name which has 31 or more characters" do
     user = User.new(
       name: "test"*10,
-      summary: "test_summary2"
+      email: "test@example.com",
+      password: "000000",
+      password_confirmation: "000000"
     )
     user.valid?
     expect(user.errors[:name]).to include("は30文字以内で入力してください")
   end
-  # 概要が256文字以上なら無効な状態であること
-  it "is invalid with summary which has 256 or more characters" do
+  # パスワードが5文字以下なら無効な状態であること
+  it "is invalid with password which has 5 or less characters" do
     user = User.new(
-      name: "test_name",
-      summary: "test"*100
+      name: "test",
+      email: "test@example.com",
+      password: "0000",
+      password_confirmation: "0000"
     )
     user.valid?
-    expect(user.errors[:summary]).to include("は255文字以内で入力してください")
+    expect(user.errors[:password]).to include("は6文字以上で入力してください")
   end
-  # URLの書式が有効な状態であること
-  it "is valid with link which is collect format" do
+  # パスワードと確認パスワードが一致しなければ無効な状態であること
+  it "is invalid password and password_confimation do not match" do
     user = User.new(
-      name: "test_name",
-      summary: "test_summary",
-      link: "https://www.amazon.co.jp/"
+      name: "test",
+      email: "test@example.com",
+      password: "000000",
+      password_confirmation: "111111"
     )
-    expect(user).to be_valid
+    expect(user).not_to be_valid
   end
-  # URLの書式が無効な状態であること
-  it "is invalid with link which is incollect format" do
+  # メールアドレスの書式が無効な状態であること
+  it "is valid with email which is collect format" do
     user = User.new(
       name: "test_name",
-      summary: "test_summary",
-      link: "amazon"
+      email: "test_email",
+      password: "000000",
+      password_confirmation: "000000"
     )
     user.valid?
-    expect(user.errors[:link]).to include("は不正な値です")
+    expect(user.errors[:email]).to include("は不正な値です")
   end
 end
