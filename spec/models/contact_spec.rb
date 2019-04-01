4a0098258c6492124c934720d8ea1835d086d663
ddgg7755 <ddgg7755@gmail.com>
2019-04-01 21:12:19 +0900

    コメントの修正

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -18,25 +18,25 @@ RSpec.describe Contact, type: :model do
     contact = @contact
     expect(contact).to be_valid
   end
-  # 名前がなければ無効な状態であること
+  # タイトルがなければ無効な状態であること
   it "is invalid without a title" do
     contact = FactoryBot.build(:contact, title: nil, user: @user)
     contact.valid?
     expect(contact.errors[:title]).to include("を入力してください")
   end
-  # 名前が31文字以上なら無効な状態であること
+  # タイトルが31文字以上なら無効な状態であること
   it "is invalid with title which has 51 or more characters" do
     contact = FactoryBot.build(:contact, title: "test"*20, user: @user)
     contact.valid?
     expect(contact.errors[:title]).to include("は50文字以内で入力してください")
   end
-  # 内容が5001文字以上なら無効な状態であること
+  # 内容が501文字以上なら無効な状態であること
   it "is invalid with message which has 501 or more characters" do
     contact = FactoryBot.build(:contact, message: "test"*200, user: @user)
     contact.valid?
     expect(contact.errors[:message]).to include("は500文字以内で入力してください")
   end
-  # URLの書式が無効な状態であること
+  # メールアドレスの書式が無効な状態であること
   it "is invalid with email which is incollect format" do
     contact = FactoryBot.build(:contact, email: "test", user: @user)
     contact.valid?
