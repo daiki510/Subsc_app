763bb052f4b4f6124b0edcf6f4affa72991b2009
ddgg7755 <ddgg7755@gmail.com>
2019-04-02 13:16:41 +0900

    マイページの料金順と支払日順に対してページネーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,8 @@ class UsersController < ApplicationController
     #ソート機能
     @subscriptions = @subscriptions.order(name: :asc) if params[:sort_name] #名前順
     @subscriptions = only_not_has_detail(@subscriptions, @details) if params[:sort_not_has_detail] #詳細未登録のみ
-    
-    # @detail_ids = @details.order(charge: :desc).pluck(:subscription_id)
-    @subscriptions = sort_charge(@details) if params[:sort_charge] #利用料金順
-    # raise
-    @subscriptions = sort_date(@details) if params[:sort_date] #支払日順
+    @subscriptions = Kaminari.paginate_array(sort_charge(@details)).page(params[:page]).per(PER) if params[:sort_charge] #利用料金順
+    @subscriptions = Kaminari.paginate_array(sort_date(@details)).page(params[:page]).per(PER) if params[:sort_date] #支払日順
     
     #ページネーション
     @subscriptions = @subscriptions.page(params[:page]).per(PER)
@@ -29,7 +26,7 @@ class UsersController < ApplicationController
 
   private
   #未登録のサブスクリプションを抽出
-  def only_not_has_detail(subscriptions, details)
+  def only_has_no_detail(subscriptions, details)
     subsc_ids = subscriptions.map {|subsc| subsc.id} #ユーザーが追加したサブスクリプションのidを抽出
     detail_ids = details.map {|detail| detail.subscription_id} #詳細が登録されているサブスクリプションのidを抽出
     not_has_detail_ids = subsc_ids - detail_ids #両方の配列にないidを算出する
