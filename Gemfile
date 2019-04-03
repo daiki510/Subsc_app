0d5349757bf8495eea80bdeed8f5686f3f7f5eb2
ddgg7755 <ddgg7755@gmail.com>
2019-04-03 21:28:52 +0900

    サブスクリプションの追加・解除ボタンに対してAjax実装

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'jquery-rails'
 
 #ログイン機能
 gem 'devise'
--- app/controllers/additions_controller.rb
+++ app/controllers/additions_controller.rb
@@ -1,20 +1,21 @@
 class AdditionsController < ApplicationController
   def create
     @addition = current_user.additions.create(subscription_id: params[:subscription_id])
-    redirect_to subscriptions_path, notice: "「#{@addition.subscription.name}」を追加しました"
+    @added_subscription = Subscription.find_by(id: @addition.subscription_id)
+    # redirect_to subscriptions_path, notice: "「#{@addition.subscription.name}」を追加しました"
   end
 
   def destroy
     @addition = Addition.find_by(subscription_id: params[:subscription_id], user_id: current_user.id)
-    @subscription = Subscription.find_by(id: @addition.subscription_id)
+    @added_subscription = Subscription.find_by(id: @addition.subscription_id)
     @addition.destroy
-    if @subscription.status == "secret"
-      @subscription.destroy
-      redirect_to user_path(current_user.id), notice: "サブスクリプションを利用一覧から外しました"
-    elsif params[:back_to_mypage] #共有サブスクリプションは削除しない場合
-      redirect_to user_path(current_user.id), notice: "サブスクリプションを利用一覧から外しました"
-    else
-      redirect_to subscriptions_path, notice: "サブスクリプションを利用一覧から外しました"
+    if @added_subscription.status == "secret"
+      @added_subscription.destroy
+    #   redirect_to user_path(current_user.id), notice: "サブスクリプションを利用一覧から外しました"
+    # elsif params[:back_to_mypage] #共有サブスクリプションは削除しない場合
+      # redirect_to user_path(current_user.id), notice: "サブスクリプションを利用一覧から外しました"
+    # else
+    #   redirect_to subscriptions_path, notice: "サブスクリプションを利用一覧から外しました"
     end
   end
 end
