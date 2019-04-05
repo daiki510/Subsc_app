7daec8ed915e40eb6530e097654686913124669e
ddgg7755 <ddgg7755@gmail.com>
2019-04-05 16:40:12 +0900

    modelスペックのテストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,16 +3,22 @@ FactoryBot.define do
     name { "test_user1" }
     email { "user1@example.com" }
     password { "000000" }
+    provider { "google" }
+    uid { "110033013805621465000" }
   end
   factory :user2, class: User do
     name { "test_user2" }
     email { "user2@example.com" }
     password { "000000" }
+    provider { "google" }
+    uid { "110033013805621465011" }
   end
   factory :admin_user, class: User do
     name { "admin" }
     email { "admin@example.com" }
     password { "000000" }
     admin { "true" }
+    provider { "google" }
+    uid { "110033013805621465022" }
   end
 end
