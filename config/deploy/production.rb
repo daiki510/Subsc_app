1dc4b62b0b265d5f1a4ba380c94d648e941ce6f0
ddgg7755 <ddgg7755@gmail.com>
2019-04-05 11:20:11 +0900

    servernameを取得したドメインへ変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '13.115.66.205', user: 'app', roles: %w{app db web}
+server 'subsc-app.tk', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/ddgg7755/.ssh/id_rsa'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: '13.115.66.205' }
+  config.action_mailer.default_url_options = { host: 'subsc-app.tk' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['GMAIL_USERNAME'],
