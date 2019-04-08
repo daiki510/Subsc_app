bc41d8ec69d2a144d0a9641ede1315cd6760a055
ddgg7755 <ddgg7755@gmail.com>
2019-04-08 12:29:42 +0900

    登録したサブスクリプションの合計件数を表示

--- app/helpers/details_helper.rb
+++ app/helpers/details_helper.rb
@@ -3,4 +3,9 @@ module DetailsHelper
   def subsc_name(detail)
     Subscription.find_by(id: detail.subscription_id).name
   end
+
+  # 登録したサブスクリプションの合計件数を算出
+  def subsc_count(subscriptions)
+    subscriptions.map {|subscription| Detail.find_by(subscription_id: subscription.id)}.count
+  end
 end
