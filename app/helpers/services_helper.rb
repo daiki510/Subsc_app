570ab23e53cd0c77bcf4855e6709e7f1dd6718f7
ddgg7755 <ddgg7755@gmail.com>
2019-04-09 22:32:02 +0900

    ビュー内のコードをヘルパーへ定義

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,10 +1,14 @@
 module ServicesHelper
-  def total_subsc(services)
+  def total_services(services)
     services.count
   end
 
   # 追加されたサブスクリプションかどうか
   def already_added?(service)
     current_user.subscriptions.where(service_id: service.id).present?
+  end
+
+  def categories
+    Category.all
   end
 end
