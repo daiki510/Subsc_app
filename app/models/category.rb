969d98411f80719d64a5d05598821c4a563c3cbe
ddgg7755 <ddgg7755@gmail.com>
2019-04-10 16:38:28 +0900

    不要なコメントの削除

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,6 @@
 class Category < ApplicationRecord
-  # バリデーション
   validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
 
-  # アソシエーション
   has_many :categorizings, dependent: :destroy
   has_many :services, through: :categorizings, source: :service
 end
