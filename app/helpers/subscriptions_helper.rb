94480e9c12f5e551c262f3e3e60ee3932a0f9462
ddgg7755 <ddgg7755@gmail.com>
2019-04-10 12:04:23 +0900

    サブスクリプション詳細機能の修正

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,8 +1,7 @@
 module SubscriptionsHelper
-  # 詳細登録画面に紐づいているサブスクリプション名を表示
-  # def service_name(subscription)
-  #   Service.find_by(id: subscription.service_id).name
-  # end
+  def service(subscription)
+    Service.find_by(id: subscription.service_id)
+  end
 
   # 登録したサブスクリプションの合計件数を算出
   def total_subscriptions(services)
