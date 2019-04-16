10c0a1c37c6524552c4d17d32ea0733d6a2d247d
ddgg7755 <ddgg7755@gmail.com>
2019-04-16 14:37:59 +0900

    chargeの最大値の修正

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :service
 
-  validates :charge, presence: true, length: { maximum: 10 }, numericality: { greater_than_or_equal_to: 0, less_than: 1_000_000 }
+  validates :charge, presence: true, length: { maximum: 10 }, numericality: { greater_than_or_equal_to: 0, less_than: 1_000_00 }
   validates :due_date, presence: true, length: { maximum: 10 }
   validates :note, length: { maximum: 255 }
   validates :service_id, uniqueness: { scope: :user_id }
