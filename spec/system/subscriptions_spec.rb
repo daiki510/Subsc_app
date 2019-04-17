2c86edbb0dcfc881b401823576cb9e491b1e185a
ddgg7755 <ddgg7755@gmail.com>
2019-04-17 13:39:04 +0900

    subscription 検索・ソート機能のテスト追加

--- spec/system/subscriptions_spec.rb
+++ spec/system/subscriptions_spec.rb
@@ -5,7 +5,11 @@ describe 'サブスクリプション管理機能', type: :system do
   let(:admin_user) { FactoryBot.create(:admin_user) }
   let!(:service_01) { FactoryBot.create(:service, user: admin_user, name: 'testname_01') }
   let!(:service_02) { FactoryBot.create(:service, user: admin_user, name: 'testname_02') }
+  let!(:service_03) { FactoryBot.create(:service, user: admin_user, name: 'testname_03') }
+  let!(:service_04) { FactoryBot.create(:service, user: admin_user, name: 'testname_04') }
   let!(:subscription_02) { FactoryBot.create(:subscription, user: user, service: service_02, charge: 2000, due_date: 20) }
+  let!(:subscription_03) { FactoryBot.create(:subscription, user: user, service: service_03, charge: 3000, due_date: 30) }
+  let!(:subscription_04) { FactoryBot.create(:subscription, user: user, service: service_04, charge: 0, due_date: 1) }
   before do
     visit new_user_session_path
     fill_in 'メールアドレス', with: login_user.email
@@ -76,7 +80,6 @@ describe 'サブスクリプション管理機能', type: :system do
         it '編集に失敗した時、編集画面にリダイレクトされる' do
           fill_in '利用料金',	with: ''
           click_button '更新する'
-          # byebug
           expect(current_path).to eq edit_subscription_path(subscription_02.id)
         end
       end
@@ -96,6 +99,74 @@ describe 'サブスクリプション管理機能', type: :system do
         it 'マイページに削除メッセージが表示される' do
           page.accept_confirm '本当に解除してもいいですか?'
           expect(page).to have_selector '.alert', text: '「testname_02」を利用一覧から外しました'
+        end
+      end
+    end
+    describe '検索機能' do
+      context '任意のサービス名を検索' do
+        it '検索にヒットすると、一覧画面に表示される' do
+          page.find('.search-form').set('testname_03')
+          click_button '検索'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_03'
+        end
+        it '大文字でも小文字でも検索できる' do
+          page.find('.search-form').set('TESTNAME_03')
+          click_button '検索'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_03'
+        end
+        it '検索結果がない場合は、サブスクリプションが0になる' do
+          page.find('.search-form').set('testname_100')
+          click_button '検索'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions.count).to be 0
+        end
+      end
+    end
+    describe 'ソート機能' do
+      context '名前順にソート' do
+        it 'ABC順にソートされる' do
+          click_link '名前順'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_02'
+          expect(subscriptions[1]).to have_content 'testname_03'
+          expect(subscriptions[2]).to have_content 'testname_04'
+        end
+      end
+      context '新着順にソート' do
+        it '作成日が新しい順にソートされる' do
+          click_link '新着順'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_04'
+          expect(subscriptions[1]).to have_content 'testname_03'
+          expect(subscriptions[2]).to have_content 'testname_02'
+        end
+      end
+      context '利用金額にソート' do
+        it '利用金額の多い順にソートされる' do
+          click_link '利用料金順'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_03'
+          expect(subscriptions[1]).to have_content 'testname_02'
+          expect(subscriptions[2]).to have_content 'testname_04'
+        end
+      end
+      context '支払日にソート' do
+        it '支払日が新しい順にソートされる' do
+          click_link '支払日順'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_04'
+          expect(subscriptions[1]).to have_content 'testname_02'
+          expect(subscriptions[2]).to have_content 'testname_03'
+        end
+      end
+      context '未登録のみにソート' do
+        it '未登録のみにソートされる' do
+          click_link '未登録のみ'
+          subscriptions = page.all('h5.service-title')
+          expect(subscriptions[0]).to have_content 'testname_04'
+          expect(subscriptions.count).to be 1
         end
       end
     end
