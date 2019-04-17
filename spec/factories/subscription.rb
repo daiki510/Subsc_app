ff3ed0175e0cef0a6289d9b483b399af837d1be0
ddgg7755 <ddgg7755@gmail.com>
2019-04-17 13:07:11 +0900

    subscription 詳細・編集・削除機能のテスト追加

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription do
     charge { '1000' }
-    due_date { '1日' }
+    due_date { '1' }
     note { 'this is test note' }
     user
     service
--- spec/system/subscriptions_spec.rb
+++ spec/system/subscriptions_spec.rb
@@ -5,7 +5,7 @@ describe 'サブスクリプション管理機能', type: :system do
   let(:admin_user) { FactoryBot.create(:admin_user) }
   let!(:service_01) { FactoryBot.create(:service, user: admin_user, name: 'testname_01') }
   let!(:service_02) { FactoryBot.create(:service, user: admin_user, name: 'testname_02') }
-  # let!(:subscription_01) { FactoryBot.create(:subscription, user: user, service: service_01) }
+  let!(:subscription_02) { FactoryBot.create(:subscription, user: user, service: service_02, charge: 2000, due_date: 20) }
   before do
     visit new_user_session_path
     fill_in 'メールアドレス', with: login_user.email
@@ -31,8 +31,6 @@ describe 'サブスクリプション管理機能', type: :system do
         click_button '登録する'
         visit user_path(user.id)
         expect(page).to have_selector 'h5.service-title', text: 'testname_01'
-        expect(page).to have_selector 'span.charge-item', text: '1000円'
-        expect(page).to have_selector 'span.due-date-item', text: '10日'
       end
       it '登録に失敗した時、登録画面にリダイレクトされる' do
         fill_in '利用料金',	with: ''
@@ -41,34 +39,64 @@ describe 'サブスクリプション管理機能', type: :system do
       end
     end
   end
-  # save_and_open_page
-  xdescribe '一覧表示機能' do
-    context 'サービスを追加している場合' do
+  describe 'マイページのテスト' do
+    let(:login_user) { user }
+    before do
+      visit user_path(user.id)
+    end
+    describe '一覧表示機能' do
       it '利用料金が表示されている' do
+        expect(page).to have_selector 'span.charge-item', text: '2000円'
       end
       it '支払日が表示されている' do
+        expect(page).to have_selector 'span.due-date-item', text: '20日'
       end
     end
-  end
-  xdescribe '詳細機能' do
-    context 'サービスを追加している場合' do
+    describe '詳細機能' do
+      before do
+        find('.subsc-info-1').click
+      end
       it '利用料金が表示されている' do
+        expect(page).to have_selector 'h5.charge-text', text: '2000円'
       end
       it '支払日が表示されている' do
+        expect(page).to have_selector 'h5.due-date-text', text: '毎月 20日'
       end
     end
-  end
-  xdescribe '編集機能' do
-    context '金額を変更する' do
-      it '編集に成功した時、金額の表示が変更される' do
+    describe '編集機能' do
+      before do
+        find('.subsc-edit-1').click
       end
-      it '編集に失敗した時、編集画面にリダイレクトされる' do
+      context '金額を変更する' do
+        it '編集に成功した時、金額の表示が変更される' do
+          fill_in '利用料金',	with: 3000
+          click_button '更新する'
+          expect(page).to have_selector 'span.charge-item', text: '3000円'
+        end
+        it '編集に失敗した時、編集画面にリダイレクトされる' do
+          fill_in '利用料金',	with: ''
+          click_button '更新する'
+          # byebug
+          expect(current_path).to eq edit_subscription_path(subscription_02.id)
+        end
+      end
+      context '支払日を変更する' do
+        it '編集に成功した時、支払日の表示が変更される' do
+          select 30, from: 'subscription[due_date]'
+          click_button '更新する'
+          expect(page).to have_selector 'span.due-date-item', text: '30日'
+        end
       end
     end
-  end
-  xdescribe '削除機能' do
-    context '削除ボタンを押す' do
-      it 'マイページのサブスクリプション一覧から表示されなくなる' do
+    describe '削除機能' do
+      before do
+        find('.subsc-delete-1').click
+      end
+      context '削除ボタンを押す' do
+        it 'マイページに削除メッセージが表示される' do
+          page.accept_confirm '本当に解除してもいいですか?'
+          expect(page).to have_selector '.alert', text: '「testname_02」を利用一覧から外しました'
+        end
       end
     end
   end
