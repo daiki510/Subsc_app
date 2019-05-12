8908ba1a7a8c744a80950c334d67f48c5a62bd7c
ddgg7755 <ddgg7755@gmail.com>
2019-05-12 10:03:06 +0900

    気になる機能のAJax化

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,12 +1,14 @@
 class ClipsController < ApplicationController
   def create
     @clip = current_user.clips.create(service_id: params[:service_id])
-    redirect_back(fallback_location: root_path)
+    @service = Service.find(@clip.service_id)
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @clip = Clip.find_by(service_id: params[:service_id], user_id: current_user.id)
+    @service = Service.find(@clip.service_id)
     @clip.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 end
