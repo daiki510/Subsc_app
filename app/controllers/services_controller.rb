c6531ee19b4c216d066ba7acbbf561a7f1269275
ddgg7755 <ddgg7755@gmail.com>
2019-05-12 10:13:41 +0900

    「気になる」ソートボタンを追加

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -20,6 +20,7 @@ class ServicesController < ApplicationController
     @services = @services.sort_update if params[:sort_update] # 更新順
     @services = @services.sort_with_user_count if params[:sort_with_rank] # 人気順
     @services = @services.search_with_using(current_user) if params[:search_with_using] # 利用中のみ
+    @services = current_user.clipped_services if params[:sort_clipped] # 気になるサービスのみ
     @services = Service.search_secret_status.search_with_user_id(current_user) if params[:sercet_index] # オリジナルのみ
 
     # CSV出力
